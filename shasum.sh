c76b6602645ba384bdf0b5f636611ceb593509bb
Haamieh <sina.adineh@yahoo.com>
2022-06-22 02:42:11 +0000

    added date to shasum

--- shasum.sh
+++ shasum.sh
@@ -1,1 +1,17 @@
+#!/usr/bin/env bash
+
 # this will be a script
+# echo "hello world $1"
+date
+today=$(date +%d)
+
+echo "$@"
+echo "$#"
+
+if [ "$today" -gt 21 ];then
+    echo "We are in 4 th week"
+else
+    echo "We are not in 4th week"
+fi
+
+echo $today
